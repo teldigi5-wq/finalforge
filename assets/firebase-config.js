535c6b5776cdfbdddb3e2ee63b76a9b6d8b0a8ae
Poojana Kaveesh Sellahewa <teldigi5@gmail.com>
2026-09-24 17:12:21 +0530

    chore: add Firebase client config template

---
+++ assets/firebase-config.js
@@ -0,0 +1,14 @@
+// FinalForge V5 Firebase public client configuration.
+// Firebase web config values are identifiers, not your admin/service-account secrets.
+window.FINALFORGE_FIREBASE = {
+  enabled: false,
+  adminEmail: "SellahewaPK@my.sliit.lk",
+  config: {
+    apiKey: "REPLACE_ME",
+    authDomain: "REPLACE_ME.firebaseapp.com",
+    projectId: "REPLACE_ME",
+    storageBucket: "REPLACE_ME.appspot.com",
+    messagingSenderId: "REPLACE_ME",
+    appId: "REPLACE_ME"
+  }
+};
